111b21fb74be69b7ade66928a64a9fd1f201ef9c
league <jack.stewart.hay@gmail.com>
2024-07-12 12:00:16 -0700

    july 12

--- _02_random/_4_validation/validation.py
+++ _02_random/_4_validation/validation.py
@@ -8,6 +8,28 @@ if __name__ == '__main__':
     random_number = random.randint(1, 5)
 
     print(random_number)
+    for i in range(10):
+        if random_number == 1:
+            messagebox.showinfo(message='you are smart')
+        if random_number == 2:
+            messagebox.showinfo(message='you are nice')
+        if random_number == 3:
+            messagebox.showinfo(message='you are decent')
+        if random_number == 4:
+            messagebox.showinfo(message='you are smart')
+        if random_number == 5:
+            messagebox.showinfo(message='you are smart')
+        if random_number == 6:
+            messagebox.showinfo(message='you are smart')
+        if random_number == 7:
+            messagebox.showinfo(message='you are smart')
+        if random_number == 8:
+            messagebox.showinfo(message='you are smart')
+        if random_number == 9:
+            messagebox.showinfo(message='you are smart')
+        if random_number == 10:
+            messagebox.showinfo(message='you are smart')
+
 
     # TODO 1) Use each value of random_number to give the user a random compliment
 
--- _02_random/_5_magic_8_ball/magic_8_ball.py
+++ _02_random/_5_magic_8_ball/magic_8_ball.py
@@ -6,10 +6,11 @@ if __name__ == '__main__':
     window.withdraw()
 
     # TODO Get the user to enter a question for the 8 ball to answer
-
+    question = simpledialog.messagebox(title='The magic 8 ball ',prompt='ask the 8 ball a question')
     # Make a variable and initialize it to a random number between 0 and 3
-
+    a = random.randint(0,3)
     # If the random number is 0
+    if a == 0:
 
         # tell the user "Yes"
 
